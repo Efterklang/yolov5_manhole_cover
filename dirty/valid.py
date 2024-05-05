612b6162a07546eb0c6c2d53d9316be889359dd9
Efterklang <gaojiaxing2004@qq.com>
2024-05-05 19:43:26 +0800

    ultralytics

--- dirty/valid.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("/root/teamshare/weights/zhbest4.pt")
-
-# Customize validation settings
-validation_results = model.val(
-    data="./valid.yaml", imgsz=640, conf=0.25, iou=0.6, device="0"
-)
